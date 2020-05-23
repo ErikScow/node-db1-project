83677b28e718fe7c118ed043b990ffc3c3c35be7
Erik Scow <erikscow@gmail.com>
2020-05-22 18:37:49 -0700

    Added get handler for accounts using knex

--- api/server.js
+++ api/server.js
@@ -6,4 +6,16 @@ const server = express();
 
 server.use(express.json());
 
+//Request handlers:
+
+server.get('/api/accounts', (req, res) => {
+    db.select('*').from('accounts')
+        .then(accounts => {
+            res.status(200).json(accounts)
+        })
+        .catch(error => {
+            res.status(500).json({ message: 'Failed to get accounts'})
+        })
+})
+
 module.exports = server;
