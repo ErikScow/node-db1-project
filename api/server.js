494f5c55bd78bd7f9c1e13a13810e7f87cc00c34
Erik Scow <erikscow@gmail.com>
2020-05-22 19:54:06 -0700

    Added post request handler

--- api/server.js
+++ api/server.js
@@ -8,6 +8,18 @@ server.use(express.json());
 
 //Request handlers:
 
+server.post('/api/accounts', validateAccount, (req, res) => {
+    console.log(req.body)
+    db('accounts').insert(req.body)
+    .then( accountId => {
+        res.status(201).json(accountId)
+    })
+    .catch( error => {
+        console.log('could not post to database', error)
+        res.status(500).json({ message: 'could not post to database' })
+    })
+})
+
 server.get('/api/accounts', (req, res) => {
     db.select('*').from('accounts')
         .then(accounts => {
@@ -17,5 +29,36 @@ server.get('/api/accounts', (req, res) => {
             res.status(500).json({ message: 'Failed to get accounts'})
         })
 })
+
+//Middlewares:
+
+function validateAccount(req, res, next){
+    if(!req.body){
+        res.status(400).json({ message: 'missing account data'})
+    } else if (!req.body.name){
+        res.status(400).json({ message: 'missing account name data'})
+    } else if (!req.body.budget) {
+        res.status(400).json({ message: 'missing budget data'})
+    } else {
+        next()
+    }
+}
+
+function validateId(req, res, next){
+    const { id } = req.params.id
+    db('accounts').where( {id})
+        .then( account => {
+            if(account){
+                req.account = account
+                next()
+            } else {
+                res.status(400).json({ message: 'invalid account id'})
+            }
+        })
+        .catch( error => {
+           res.status(500).json({ message: 'could not retrieve from database'})
+        })
+    
+}
 
 module.exports = server;
