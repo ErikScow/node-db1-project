54b68cd19cac47cc4c7d5314b8ce5e5a6eb91a7f
Erik Scow <erikscow@gmail.com>
2020-07-22 19:39:11 -0700

    Added middleware for validating account data and ID

--- api/server.js
+++ api/server.js
@@ -6,4 +6,44 @@ const server = express();
 
 server.use(express.json());
 
+//handlers
+
+server.post()
+
+server.get()
+
+server.delete()
+
+server.put()
+
+//middleware
+
+function validateAccount(req, res, next){
+    if(!req.body){
+        res.status(400).json({ message: 'missing account data'})
+    } else if (!req.body.name){
+        res.status(400).json({ message: 'missing account name data'})
+    } else if (!req.body.budget) {
+        res.status(400).json({ message: 'missing budget data'})
+    } else {
+        next()
+    }
+}
+
+function validateId(req, res, next){
+    const { id } = req.params.id
+    db('accounts').where( {id})
+        .then( account => {
+            if(account){
+                req.account = account
+                next()
+            } else {
+                res.status(400).json({ message: 'invalid account id'})
+            }
+        })
+        .catch( error => {
+           res.status(500).json({ message: 'could not retrieve from database'})
+        })
+    
+}
 module.exports = server;
